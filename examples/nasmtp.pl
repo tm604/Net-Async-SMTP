fc82e88b36ce2d73733fc5708ab0afaa16d7ca7e
Tom Molesworth <cpan@entitymodel.com>
2014-02-21 19:41:44 +0000

    Use NaSMTP lookup instead of doing it ourselves.

--- examples/nasmtp.pl
+++ examples/nasmtp.pl
@@ -6,7 +6,7 @@ use Net::Async::SMTP;
 use Email::Simple;
 use Net::DNS;
 
-my $host = 'perlsite.co.uk';
+my $domain = 'perlsite.co.uk';
 my $user = 'nasmtp@perlsite.co.uk';
 my $email = Email::Simple->create(
 	header => [
@@ -18,27 +18,13 @@ my $email = Email::Simple->create(
 );
 warn "Will try to send this email:\n" . $email->as_string;
 
-{ # This blocking lookup should really use the IO::Async resolver
-	my $res  = Net::DNS::Resolver->new;
-	my @mx   = $res->mx('perlsite.co.uk');
-	if (@mx) {
-		foreach my $rr (@mx) {
-			$host = $rr->exchange;
-		}
-	} else {
-		warn "Can't find MX records for $host ", $res->errorstring, "\n";
-	}
-
-}
-warn "... to $user via host $host\n";
-
 my $loop = IO::Async::Loop->new;
 my $smtp = Net::Async::SMTP->new(
-	host => $host,
+	domain => $domain,
 );
 $loop->add($smtp);
 
-$smtp->connection->then(sub {
+$smtp->connected->then(sub {
 	# So the login is a separate step here. It should perhaps be done
 	# in the background via instantiation.
 	$smtp->login(
